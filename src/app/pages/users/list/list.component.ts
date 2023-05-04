53c8066da8a023fc2c91cf1f2273dad21c395a57
salehlou <saeed.softco@gmail.com>
2023-05-04 19:24:14 +0330

    get users list

--- src/app/pages/users/list/list.component.ts
+++ src/app/pages/users/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
 
 import { UserService } from 'src/app/core/services/user/user.service';
 import { BaseResponseModel } from 'src/app/domain/models/base-response.model';
@@ -12,26 +13,30 @@ import { UserModel } from 'src/app/domain/models/user.model';
 })
 export class ListComponent {
 
-  displayedColumns: string[] =
-    [
-      'firstName',
-      'lastName',
-      'age',
-      'gender',
-      'email',
-      'phone',
-      'birthDate',
-      'eyeColor'
-    ];
+  displayedColumns: string[] = ['firstName', 'lastName', 'age', 'gender', 'email', 'phone', 'birthDate', 'eyeColor'];
 
-  dataSource!: UserModel[];
+
+  dataSource = new MatTableDataSource();
 
   constructor(private userService: UserService) { }
 
+  repos!: UserModel[];
+
+  loading: boolean = false;
+  errorMessage!: string;
+
   ngOnInit() {
 
-    let data = this.userService.get();
-    // this.dataSource = data.users;
+    let data = this.userService.get()
+      .subscribe(
+        (response) => {
+          this.repos = response.users as UserModel[];
+          this.dataSource.data = this.repos;
+        });
+  }
+
+  search(searchTerm: string) {
+    this.dataSource.filter = searchTerm.trim().toLowerCase();
 
   }
 }
